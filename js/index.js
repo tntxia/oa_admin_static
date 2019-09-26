216a5ce830c46dbdcdd2a49e97ab127d4c759ff6
tntxia <tntxia@163.com>
2019-09-26 21:59:33 +0800

    OA管理

--- js/index.js
+++ js/index.js
@@ -1,82 +1,21 @@
 $(function() {
 
-    loadWebConfig().then(() => {
-        installMainLayout().then((mainLayout) => {
-            init(mainLayout);
-        });
-        return;
-
-        var target = "";
-
-        // 绑定路由，设置路由变化影响的区域
-        router.register({
-            target: $("#mainPage"),
-            defaultModule: 'main'
-        });
-
-        new Vue({
-            el: '#changePassModal',
-            data: {
-                pass: '',
-                passConfirm: ''
-            },
-            methods: {
-                updatePass: function() {
-
-                    var pass = this.pass;
-                    var passConfirm = this.passConfirm;
-
-                    if (pass != passConfirm) {
-                        alert("两次输入的密码不一致！");
-                        return;
-                    }
-
-                    $.ajax({
-                        url: 'modifyPass.do',
-                        data: {
-                            pass: pass
-                        }
-                    }).done(function(data) {
-                        if (data.success) {
-                            alert("修改成功");
-                        } else {
-                            alert("修改失败");
-                        }
-                    }).fail(function(e) {
-                        alert("修改异常");
-                    });
-                }
-            }
-        });
-    });
-
-    function loadWebConfig() {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: '/oa_admin_static/json/webConfig.json'
-            }).done(res => {
-                WebInstall.install(res).then(() => {
-                    resolve();
-                }, e => {
-                    reject(e);
-                });
-            }).fail(e => {
-                console.error(e);
-                reject(e);
-            })
-        });
-    }
-
-    function installMainLayout() {
-        $(document.body).empty();
-        return MainLayoutInstall.install({
+    WebInstall.install({
+        configUrl: '/oa_admin_static/json/webConfig.json',
+        mainLayout: {
             leftbarWidth: 150,
             headerHeight: 50,
             headerTemplate: '/oa_admin_static/template/header.html',
-            mainSecTemplate: '/oa_admin_static/template/content.html',
-            dialogs: []
-        });
-    }
+            mainSecTemplate: '/oa_admin_static/template/content.html'
+        }
+    }).then(function(webApp) {
+        window.mainLayout = webApp.mainLayout;
+        $(document.body).append($("<div>", {
+            text: '组件加载完成，开始渲染'
+        }));
+        init(mainLayout);
+
+    })
 
     function init(mainLayout) {
         $.getJSON("/oa_admin_static/json/menu.json").done(function(res) {
